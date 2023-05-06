19bf2a340e1f422c47fb35946f045108dc7b1ef3
lzear <antoine.clausse@gmail.com>
2023-05-07 00:20:53 +0200

    ☢️ delete some stuff?

--- v1/packages/gtfs-loader/src/csv/csv-utils.ts
+++
@@ -1,6 +0,0 @@
-export const LINE_LENGTH = 40
-export const log2cols = (
-  left: string,
-  right: string,
-  lineLength: number = LINE_LENGTH,
-) => console.log(left + right.padStart(lineLength - left.length))
