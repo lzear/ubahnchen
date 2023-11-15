64f9767b0cb0d28aefc0e62756d41a7c0f866718
lzear <antoine.clausse@gmail.com>
2023-11-16 00:08:39 +0100

    🤓 add cli command

---
+++ libs/cli/src/bin/index.ts
@@ -0,0 +1,74 @@
+#!/usr/bin/env -S node --no-warnings --loader ts-node/esm --es-module-specifier-resolution=node
+
+import { Command } from '@commander-js/extra-typings'
+import chalk from 'chalk'
+
+import { citiesList, isCity } from '@ubahnchen/cities'
+import {cityStats, logCityStats} from '@ubahnchen/cities/node'
+import {
+  downloadCity,
+  gtfsToSqlite,
+  gtfsUrlToSqlite,
+} from '@ubahnchen/gtfs-to-sqlite'
+
+const pckg = await import('../../package.json', {
+  assert: {
+    type: 'json',
+  },
+})
+
+const program = new Command()
+
+const getCities = (city: string | undefined) => {
+  if (!city) return [...citiesList]
+  if (isCity(city)) return [city]
+  throw new Error(`City ${city} is not supported`)
+}
+
+program
+  .name('ubahnchen')
+  .description('Internal CLI for the ubahnchen project')
+  .version(pckg.version)
+
+program
+  .command('cities')
+  .description('List all cities')
+  .action(() => console.log(citiesList.join('\t')))
+
+program
+  .command('download')
+  .description(
+    'Download and extract the GTFS data for a city. Skipping if the files are already up to date.',
+  )
+  .option('-c, --city <city>', 'City name')
+  .option('-f, --force', 'Rewrite existing file')
+  .action(async ({ city, force }) => {
+    const cities = getCities(city)
+    for (const c of cities) await downloadCity({ city: c, force: !!force })
+  })
+
+program
+  .command('to-sqlite')
+  .description('Store the GTFS CSV data into a SQLite database.')
+  .option('-c, --city <city>', 'City name')
+  .action(async ({ city }) => {
+    const cities = getCities(city)
+    for (const c of cities) await gtfsToSqlite({ city: c }, false)
+  })
+
+program
+  .command('stats')
+  .alias('stat')
+  .description('Show the status of the ubahnchen assets')
+  .option('-c, --city <city>', 'City name')
+  .action(async ({ city }) => {
+    const cities = getCities(city)
+    const stats = await Promise.all(cities.map((city) => cityStats(city)))
+    for (const stat of stats) {
+      console.log()
+      await logCityStats(stat)
+    }
+    console.log()
+  })
+
+program.parse()
