9d3cc28bf9cd94f7a749bc06f7d12e946d318096
lzear <antoine.clausse@gmail.com>
2024-01-29 00:00:51 +0100

    🧑‍🔬 add tests on some `@ubahnchen/utils`

--- packages/utils/src/perf-function.ts
+++ packages/utils/src/perf-function.ts
@@ -1,5 +1,3 @@
-import { humanizeTime } from './humanize-time.js'
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export class PerfFunction<F extends (...arguments_: any[]) => any> {
   public executions: number[] = []
@@ -7,11 +5,14 @@ export class PerfFunction<F extends (...arguments_: any[]) => any> {
 
   constructor(private fn: F) {
     // @ts-ignore
-    this.wrapped = async (...arguments_: Parameters<F>): Promise<ReturnType<F>> => {
+    this.wrapped = async (
+      ...arguments_: Parameters<F>
+    ): Promise<ReturnType<F>> => {
       const start = performance.now()
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       const result: ReturnType<F> = await this.fn(...arguments_)
       this.executions.push(performance.now() - start)
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
       return result
     }
   }
@@ -37,17 +38,5 @@ export class PerfFunction<F extends (...arguments_: any[]) => any> {
 
   public get averagePerS() {
     return 1 / this.averageS
-  }
-
-  public displayHuman() {
-    console.log(
-      `${this.fn.name} : ${this.calls} calls, ${humanizeTime(this.time)} total, ${humanizeTime(this.average)} average`,
-    )
-  }
-
-  public display() {
-    console.log(
-      `${this.fn.name} : ${this.calls} calls, ${this.time} total, ${this.average} average`,
-    )
   }
 }
