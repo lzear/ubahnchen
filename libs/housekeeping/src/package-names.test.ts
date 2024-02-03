1df110bfb3756e72d1fced6fd8a475eed5be4799
lzear <antoine.clausse@gmail.com>
2024-02-03 23:27:44 +0100

    🏡 housekeeping

--- libs/housekeeping/src/package-names.test.ts
+++ libs/housekeeping/src/package-names.test.ts
@@ -1,32 +1,67 @@
 import { describe, it } from '@jest/globals'
 
-import { projectRoot } from '@ubahnchen/paths'
+import { P } from '@ubahnchen/cities/node'
 
-// import { listDirectories } from './packages'
-//
-// console.log('🦺 antoinelog 111', 111)
-//
-// const getPackageJson = async (workspace: string, directory: string) => {
-//   const { default: package_ } = (await import(
-//     `${projectRoot}/${workspace}/${directory}/package.json`
-//   )) as { default: Record<string, any> }
-//   return package_
-// }
+import { listDirectories } from './packages'
 
-describe('namings', () => {
-  describe.each(['packages', 'libs', 'apps'])('%p', (workspace) => {
-    // const directories: string[] = listDirectories(workspace)
+const WORKSPACES = ['packages', 'libs', 'apps']
 
-    describe.each(['sdf'])('%p', (directory) => {
-      // it('should be named correctly', async () => {
-      //   const package_ = await getPackageJson(workspace, directory)
-      //   expect(package_.name).toBe(`@ubahnchen/${directory}`)
-      // })
+const getPackageJson = async (workspace: string, directory: string) => {
+  const { default: package_ } = (await import(
+    `${P.PROJECT_ROOT}/${workspace}/${directory}/package.json`
+  )) as { default: Record<string, unknown> }
+  return package_
+}
+
+describe('workspaces', () => {
+  it('lints', async () => {
+    const lintScripts = await Promise.all(
+      WORKSPACES.flatMap((workspace) =>
+        listDirectories(workspace).map(async (directory) => {
+          const package_ = await getPackageJson(workspace, directory)
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+          // @ts-ignore
+          return package_.scripts?.lint as string
+        }),
+      ),
+    )
+    expect(lintScripts).toMatchInlineSnapshot(`
+      [
+        "eslint . --ext js,ts,jsx,tsx,cjs,json,md",
+        "eslint . --ext js,ts,jsx,tsx,cjs,json,md",
+        "eslint . --ext js,ts,jsx,tsx,cjs,json,md",
+        "eslint . --ext js,ts,jsx,tsx,cjs,json,md",
+        "eslint . --ext js,ts,jsx,tsx,cjs,json,md",
+        "eslint . --ext js,ts,jsx,tsx,cjs,json,md",
+        "eslint . --ext js,ts,jsx,tsx,cjs,json,md",
+        "eslint . --ext js,ts,jsx,tsx,cjs,json,md",
+        "eslint . --ext js,ts,jsx,tsx,cjs,json,md",
+        "eslint . --ext js,ts,jsx,tsx,cjs,json,md",
+        "eslint . --ext js,ts,jsx,tsx,cjs,json,md",
+        "eslint . --ext js,ts,jsx,tsx,cjs,json,md",
+        "next lint",
+      ]
+    `)
+  })
+  describe.each(WORKSPACES)('%p', (workspace) => {
+    const directories: string[] = listDirectories(workspace)
+
+    describe.each(directories)('%p', (directory) => {
+      it('should be named correctly', async () => {
+        const package_ = await getPackageJson(workspace, directory)
+        expect(package_.name).toBe(`@ubahnchen/${directory}`)
+      })
 
       it('should have volta', async () => {
         expect(2).toBe(2)
-        // const package_ = await getPackageJson(workspace, directory)
-        // expect(package_.volta).toMatchObject({ extends: '../../package.json' })
+        const package_ = await getPackageJson(workspace, directory)
+        expect(package_.volta).toMatchObject({ extends: '../../package.json' })
+      })
+
+      it('should lint', async () => {
+        expect(2).toBe(2)
+        const package_ = await getPackageJson(workspace, directory)
+        expect(package_.scripts).toMatchObject({ lint: /lint/ })
       })
     })
   })
