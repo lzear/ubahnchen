4f3e5084647ac9489095847032c18ae87e083c37
lzear <antoine.clausse@gmail.com>
2024-06-04 22:44:38 +0200

    🧑‍⚖️ add license info

--- libs/cities/src/data/berlin/s/index.ts
+++ libs/cities/src/data/berlin/s/index.ts
@@ -1,4 +1,14 @@
-import type { MapConfig } from '../../types'
+import type { License, MapConfig } from '../../types'
+
+const license: License = {
+  name: 'CC BY-SA 4.0',
+  author: {
+    name: 'Arbalete',
+    url: 'https://commons.wikimedia.org/wiki/User:Arbalete',
+  },
+  source:
+    'https://commons.wikimedia.org/wiki/File:S-Bahn_Berlin_-_Netzplan.svg',
+}
 
 export const config = {
   name: 'Berlin S-Bahn',
@@ -9,4 +19,5 @@ export const config = {
   here: import.meta.url,
   stopsFilter: (stop: { stop_lat: number; stop_lon: number }) =>
     stop.stop_lat > 52 && stop.stop_lon > 13,
+  license,
 } satisfies MapConfig
--- libs/cities/src/data/berlin/su/index.ts
+++ libs/cities/src/data/berlin/su/index.ts
@@ -1,12 +1,20 @@
-import type { MapConfig } from '../../types'
+import type { License, MapConfig } from '../../types'
+
+const license: License = {
+  name: 'CC BY-SA 4.0',
+  author: {
+    name: 'S. Gollin',
+  },
+  source: 'https://de.wikipedia.org/wiki/Datei:S-_und_U-Bahn-Linien_Berlin.svg',
+}
 
 export const config = {
   name: 'Berlin U-Bahn und S-Bahn',
   file: 'U-Bahn_Berlin_-_Netzplan.svg',
-  source:
-    'https://de.m.wikipedia.org/wiki/Datei:S-_und_U-Bahn-Linien_Berlin.svg',
+  source: 'https://de.wikipedia.org/wiki/Datei:S-_und_U-Bahn-Linien_Berlin.svg',
   routeTypes: [109, 400],
   here: import.meta.url,
   stopsFilter: (stop: { stop_lat: number; stop_lon: number }) =>
     stop.stop_lat > 52 && stop.stop_lon > 13,
+  license,
 } satisfies MapConfig
